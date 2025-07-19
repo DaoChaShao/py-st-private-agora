ac2cb25adf897268546a9e6f1bcabab0c957fc42
DaoChaShao <27882272@qq.com>
2025-07-19 14:46:52 +0800

    feat: add main.py with initial structure for the application

---
+++ main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3.12
+# -*- Coding: UTF-8 -*-
+# @Time     :   2025/7/19 14:44
+# @Author   :   Shawn
+# @Version  :   Version 0.1.0
+# @File     :   main.py
+# @Desc     :   
+
+def main() -> None:
+    """ streamlit run main.py """
+    pass
+
+
+if __name__ == "__main__":
+    main()
