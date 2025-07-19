a1c073bbbb4f832c5b92f7d546f96ed915cd3c32
DaoChaShao <27882272@qq.com>
2025-07-19 18:32:12 +0800

    feat: implement window and sidebar configuration in main.py

--- main.py
+++ main.py
@@ -6,9 +6,16 @@
 # @File     :   main.py
 # @Desc     :   
 
+from utils.layout import window_setter, subpages_setter
+
+
 def main() -> None:
     """ streamlit run main.py """
-    pass
+    # Set the window configuration
+    window_setter()
+
+    # Set the subpages on the sidebar
+    subpages_setter()
 
 
 if __name__ == "__main__":
