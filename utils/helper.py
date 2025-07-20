1dcb91488632de66b973fba7d30cac3be65a3738
DaoChaShao <27882272@qq.com>
2025-07-21 01:32:58 +0800

    feat: add Timer and SeedSetter classes for performance measurement and reproducibility

--- utils/helper.py
+++ utils/helper.py
@@ -5,3 +5,76 @@
 # @Version  :   Version 0.1.0
 # @File     :   helper.py
 # @Desc     :
+
+from faker import Faker
+from random import seed as random_seed, getstate as get_state, setstate as set_state
+from time import perf_counter
+
+fake = Faker()
+
+
+class Timer(object):
+    """ timing code blocks using a context manager """
+
+    def __init__(self, description: str = None, precision: int = 5):
+        """ Initialise the Timer class
+        :param description: the description of a timer
+        :param precision: the number of decimal places to round the elapsed time
+        """
+        self._description: str = description
+        self._precision: int = precision
+        self._start: float = 0.0
+        self._end: float = 0.0
+        self._elapsed: float = 0.0
+
+    def __enter__(self):
+        """ Start the timer """
+        self._start = perf_counter()
+        print()
+        print("-" * 50)
+        print(f"{self._description} has been started.")
+        return self
+
+    def __exit__(self, *args):
+        """ Stop the timer and calculate the elapsed time """
+        self._end = perf_counter()
+        self._elapsed = self._end - self._start
+
+    def __repr__(self):
+        """ Return a string representation of the timer """
+        if self._elapsed != 0.0:
+            print("-" * 50)
+            return f"{self._description} took {self._elapsed:.{self._precision}f} seconds."
+        return f"{self._description} has NOT been started."
+
+
+class SeedSetter(object):
+    """ Set a random seed for reproducibility.
+    :param object: Object class
+    """
+
+    def __init__(self, seed: int = 9527):
+        """ Initialise the RandomSeed class with a seed. """
+        self._seed = seed
+        self._state_random = None
+        self._state_faker = None
+
+    def __enter__(self):
+        """ Enter the context manager and set the random seed. """
+        # Store the current state of random and Faker
+        self._state_random = get_state()
+        self._state_faker = fake.random.getstate()
+        # Set the random seed for reproducibility
+        random_seed(self._seed)
+        Faker.seed(self._seed)
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        """ Exit the context manager and reset the random seed. """
+        # Reset the random and Faker states to their original values.
+        set_state(self._state_random)
+        fake.random.setstate(self._state_faker)
+        return False
+
+    def __str__(self):
+        """ Return a string representation of the random seed. """
+        return f"SeedSetter with seed {self._seed}"
