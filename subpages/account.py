21393a504de419503859f60fe6c70a4033e54118
DaoChaShao <27882272@qq.com>
2025-07-20 22:08:49 +0800

    feat: create account.py with the main function and initial setup

---
+++ subpages/account.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3.12
+# -*- Coding: UTF-8 -*-
+# @Time     :   2025/7/20 22:00
+# @Author   :   Shawn
+# @Version  :   Version 0.1.0
+# @File     :   account.py
+# @Desc     :   
+
+def main() -> None:
+    """ Main Function """
+    pass
+
+
+if __name__ == "__main__":
+    main()
